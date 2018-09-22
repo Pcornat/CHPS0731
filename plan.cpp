779f68127cc8431c760e1c518b183da1a9c7ca68
Postaron <florent.denef@gmail.com>
2018-09-22 22:52:22 +0200

    Need some more research

--- plan.cpp
+++ plan.cpp
@@ -5,14 +5,24 @@
 #include "plan.hpp"
 
 bool Plan::calculIntersection(const Rayon& rayon, const Scene& scene, std::vector<Intersection>& I, int rec) {
+	float denom = glm::dot(this->normal, rayon.Vect());
+	if (denom > std::numeric_limits<float>::epsilon()) {
+		//Need the point where the normal is
+	}
 	return false;
 }
 
 Plan::Plan(const float r, const float g, const float b, float distOrig, float x, float y, float z) : Objet(r, g, b),
 																									 distOrig(distOrig),
-																									 normal(x, y, z) {}
+																									 normal(x, y, z) {
+	this->normal = glm::normalize(this->normal);
+}
 
 Plan::Plan(Material* material, float distOrig, float x, float y, float z) : Objet(material), distOrig(distOrig),
-																			normal(x, y, z) {}
+																			normal(x, y, z) {
+	this->normal = glm::normalize(this->normal);
+}
 
-Plan::Plan(float distOrig, float x, float y, float z) : distOrig(distOrig), normal(x, y, z) {}
+Plan::Plan(float distOrig, float x, float y, float z) : distOrig(distOrig), normal(x, y, z) {
+	this->normal = glm::normalize(this->normal);
+}
