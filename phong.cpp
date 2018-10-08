f755199cf9822b94f0a4ba14b6e90835a3acba39
Florent Denef <florent.denef@gmail.com>
2018-10-08 14:17:50 +0200

    Reflection is done, no shadows.

--- phong.cpp
+++ phong.cpp
@@ -32,7 +32,7 @@ Phong::Phong(glm::vec3&& ka, glm::vec3&& kd, float ks, float reflection) : ka(ka
  * @return
  */
 glm::vec3 Phong::computeColour(const Intersection& I, const glm::vec3& point, const Scene& s, const Rayon& rayon, int rec) {
-	glm::vec3 diff(0, 0, 0), spec(0, 0, 0), R, L;
+	glm::vec3 diff(0, 0, 0), spec(0, 0, 0), R, L, refl(0, 0, 0);
 	for (auto&& light : s.Lights) {
 		/*
 		 * Diffus = max(N.L, 0) * Kd * Lc
@@ -43,9 +43,12 @@ glm::vec3 Phong::computeColour(const Intersection& I, const glm::vec3& point, co
 		diff += glm::max(glm::dot(I.getNormal(), L), 0.0f) * this->kd * light->getCouleur();
 		spec += light->getCouleur() * glm::pow(glm::max(glm::dot(rayon.Vect(), R), 0.0f), this->ks);
 		if (this->reflection != 0.0f) {
-			Rayon ray(std::numeric_limits<float>::epsilon() * I.getNormal() + point, glm::reflect(rayon.Vect(), I.getNormal()));
-			return this->reflection * ray.Lancer(s, --rec);
+			Rayon ray(1e-3f * I.getNormal() + point, glm::reflect(-rayon.Vect(), I.getNormal()));
+			refl += ray.Lancer(s, --rec);
 		}//*/
 	}
-	return ka + diff + spec;
+	if (this->reflection != 0.0f)
+		return this->reflection * (ka + diff + spec + refl);
+	//return this->reflection * (ka + diff + spec + refl);
+	return this->reflection * ka + diff + spec;
 }
