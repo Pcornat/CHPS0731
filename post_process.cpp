42e42b08a71a3552a9b79ef9a53cc24537958c51
Florent Denef <florent.denef@gmail.com>
2018-11-20 21:47:05 +0100

    Creating the branch. Beginning to write the function.

---
+++ post_process.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Florent on 20/11/2018.
+//
+
+#include "post_process.h"
+
+void PostProcess::toneMappingPhong() {
+
+}
---
+++ post_process.h
@@ -0,0 +1,17 @@
+//
+// Created by Florent on 20/11/2018.
+//
+
+#ifndef PROJECT_POST_PROCESS_H
+#define PROJECT_POST_PROCESS_H
+
+
+class PostProcess {
+public:
+	static void toneMappingPhong();
+
+
+};
+
+
+#endif //PROJECT_POST_PROCESS_H
