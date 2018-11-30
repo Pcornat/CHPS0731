c2c6b0bdacacbd65e0ef491c0cf6c136c4f63681
Florent Denef <florent.denef@gmail.com>
2018-11-30 09:19:23 +0100

    Corrections.

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ int main(int argc, char* argv[]) {
 	Mesh mesh;
 	if (argc != 2) {
 		std::cerr << "Nombre d'arguments insuffisant, nom d'un fichier .obj attendu." << std::endl;
-		exit(EXIT_FAILURE);
+		return EXIT_FAILURE;
 	}
 	try {
 		mesh = Mesh(new Phong(false, glm::vec3(0.1f, 0.2f, 0.1f), glm::vec3(0.6f, 0.f, 0.3f), 128.0f, 0.0f),
@@ -28,7 +28,7 @@ int main(int argc, char* argv[]) {
 					glm::vec3(0.f, 0.f, 5.f), 10, 180.f, glm::vec3(0, 1, 0));
 	} catch (std::exception& e) {
 		std::cerr << e.what() << std::endl;
-		exit(EXIT_FAILURE);
+		return EXIT_FAILURE;
 	}//*/
 
 	Sphere sphere(new Phong(false, glm::vec3(0.1f, 0.2f, 0.1f), glm::vec3(0.6f, 0.0f, 0.0f), 128.0f, 1.0f),
@@ -54,13 +54,13 @@ int main(int argc, char* argv[]) {
 			   glm::vec3(0.0f, -10.0f, 0.0f),
 			   glm::vec3(0.0f, 1.0f, 0.0f));
 	Plan closedBox(
-			new Phong(false, glm::vec3(0.3f, 0.2f, 0.2f), glm::vec3(0.3f, 0.3f, 0.3f), 128.0f, 0.0f),
+			new Phong(false, glm::vec3(0.3f, 0.2f, 0.2f), glm::vec3(0.3f, 0.3f, 0.3f), 128.0f, 0.25f),
 			glm::vec3(0.0f, 0.0f, -10.0f),
 			glm::vec3(0.0f, 0.0f, 1.0f));
-	Plan_light light(glm::vec3(0.0f, 9.0f, 5.0f), glm::vec3(1.0f, 1.0f, 1.0f),
-					 glm::vec3(-1.0f, 0.f, 0.f),
+	Plan_light light(glm::vec3(0.0f, 8.0f, -5.0f), glm::vec3(1.0f, 1.0f, 1.0f),
+					 glm::vec3(1.0f, 0.f, 0.f),
 					 glm::vec3(0.f, 0.0f, 1.0f),
-					 0.1000000000f, 5.f, 5.f);
+					 0.2000000000f, 2.f, 2.f);
 	scene.addLight(&light);
 
 	scene.addObjet(&sphere);
@@ -72,7 +72,7 @@ int main(int argc, char* argv[]) {
 	scene.addObjet(&right);
 	scene.addObjet(&closedBox);
 	scene.addObjet(&background);
-	//scene.addObjet(&mesh);
+	scene.addObjet(&mesh);
 
 	myCamera.Calculer_image(myImage, scene, 6);
 
--- plan_light.cpp
+++ plan_light.cpp
@@ -65,14 +65,15 @@ float Plan_light::computeShadow(const glm::vec3& point, const Intersection& I, c
 	 */
 	Rayon rayShadow;
 	glm::vec3 light = this->position, normal = I.getNormal(), lightDir, pointB, pointC, pointD;
-	float offset = 1e-5f,
+	float offset = 1e-4f,
 			numberHeight = this->height / this->sampleStep,
 			numberWidth = this->width / this->sampleStep,
 			nbTotal = numberHeight * numberWidth;
-	float nbOk = 0.f, distance = glm::distance(point, light);
-	const glm::mat4 rightDirMat(glm::translate(this->rightDir));
+	int nbOk = 0;
+	float distance = glm::distance(point, light);
+	const glm::mat4 rightDirMat(glm::translate(this->rightDir * this->width));
 	pointB = rightDirMat * glm::vec4(this->position.x, this->position.y, this->position.z, 1);
-	pointC = glm::translate(this->depthDir) * glm::vec4(this->position.x, this->position.y, this->position.z, 1);
+	pointC = glm::translate(this->depthDir * this->height) * glm::vec4(this->position.x, this->position.y, this->position.z, 1);
 	pointD = rightDirMat * glm::vec4(pointC.x, pointC.y, pointC.z, 1);
 	rayShadow.Orig(offset * normal + point);
 	if (Rayon(rayShadow.Orig(), glm::normalize(this->position - rayShadow.Orig())).shadowRay(sc, distance, complexite - 1)
