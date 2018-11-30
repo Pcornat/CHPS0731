f383e156a9c0b58d71419a356b94e1f36612fdd3
Florent Denef <florent.denef@gmail.com>
2018-11-30 10:01:31 +0100

    Better configuration. Bunny still black :sob:

--- main.cpp
+++ main.cpp
@@ -25,7 +25,7 @@ int main(int argc, char* argv[]) {
 	try {
 		mesh = Mesh(new Phong(false, glm::vec3(0.1f, 0.2f, 0.1f), glm::vec3(0.6f, 0.f, 0.3f), 128.0f, 0.0f),
 					std::string(argv[1]),
-					glm::vec3(0.f, 0.f, 5.f), 10, 180.f, glm::vec3(0, 1, 0));
+					glm::vec3(0.f, -4.f, 8.f), 15, 180.f, glm::vec3(0, 1, 0));
 	} catch (std::exception& e) {
 		std::cerr << e.what() << std::endl;
 		return EXIT_FAILURE;
@@ -51,13 +51,13 @@ int main(int argc, char* argv[]) {
 			   glm::vec3(-10.0f, 0.0f, 0.0f),
 			   glm::vec3(1.0f, 0.0f, 0.0f));
 	Plan floor(new Phong(false, glm::vec3(0.2f, 0.2f, 0.2f), glm::vec3(0.2f, 0.0f, 0.5f), 128.f, 0.f),
-			   glm::vec3(0.0f, -10.0f, 0.0f),
+			   glm::vec3(0.0f, -5.0f, 0.0f),
 			   glm::vec3(0.0f, 1.0f, 0.0f));
 	Plan closedBox(
 			new Phong(false, glm::vec3(0.3f, 0.2f, 0.2f), glm::vec3(0.3f, 0.3f, 0.3f), 128.0f, 0.25f),
 			glm::vec3(0.0f, 0.0f, -10.0f),
 			glm::vec3(0.0f, 0.0f, 1.0f));
-	Plan_light light(glm::vec3(0.0f, 8.0f, -5.0f), glm::vec3(1.0f, 1.0f, 1.0f),
+	Plan_light light(glm::vec3(-7.0f, 8.0f, -5.0f), glm::vec3(1.0f, 1.0f, 1.0f),
 					 glm::vec3(1.0f, 0.f, 0.f),
 					 glm::vec3(0.f, 0.0f, 1.0f),
 					 0.2000000000f, 2.f, 2.f);
@@ -74,7 +74,12 @@ int main(int argc, char* argv[]) {
 	scene.addObjet(&background);
 	scene.addObjet(&mesh);
 
-	myCamera.Calculer_image(myImage, scene, 6);
+	try {
+		myCamera.Calculer_image(myImage, scene, 6);
+	} catch (const std::exception& e) {
+		std::cerr << "Exception: " << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
 
 	/*std::time(&time);
 	ss << "out_" << std::put_time(std::localtime(&time), "%d_%m_%Y_%Hh%Mm%Ss") << ".png";
