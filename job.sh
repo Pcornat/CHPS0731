088240914e676c49e20937c4abc94941767601d4
Florent Denef <florent.denef@gmail.com>
2019-01-24 20:58:48 +0100

    New branch for corrections in the code.

---
+++ job.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH	--comment "CHPS0731"
+#SBATCH	-J "RayTracing"
+
+#SBATCH --error=job.%J.err
+#SBATCH	--output=job.%J.out
+
+#SBATCH	-p short
+#SBATCH	--time=05:00:00
+
+#SBATCH	--mem 1024
+#SBATCH -c 28
+
+#SBATCH --mail-user=<florent.denef@etudiant.univ-reims.fr>
+#SBATCH	--mail-type=ALL,TIME_LIMIT_50,TIME_LIMIT_80
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+./prog Bunny.obj
